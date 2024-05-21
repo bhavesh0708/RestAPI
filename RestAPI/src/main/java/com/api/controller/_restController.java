30781f0af26b83e5cb6a80ceaefb4f95bcdf56c2
Bhavesh Suthar <bhsuthar1998+github@gmail.com>
2024-05-21 14:27:04 +0530

    Working test

--- RestAPI/src/main/java/com/api/controller/_restController.java
+++ RestAPI/src/main/java/com/api/controller/_restController.java
@@ -1,5 +1,8 @@
 package com.api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class _restController {
 
     @GetMapping("/")
-    public String book(){
+    public ResponseEntity<?> book(){
 
-        return "Welcome to Rest Api page";
+        return ResponseEntity.status(HttpStatus.OK).body("Rest API Access Granted , Welcome");
     }
 }
